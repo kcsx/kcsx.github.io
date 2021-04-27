f155c3527034c98957012a447cd3eb5298b5845b
kaiyu <1865400033@.e.gzhu.edu.cn>
2021-04-28 00:07:45 +0800

    Site updated: 2021-04-28 00:07:45

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -263,7 +263,7 @@ function initWidget(config, apiPath) {
 		};
 	}
 	document.body.insertAdjacentHTML("beforeend", `<div id="waifu-toggle">
-			<span>看板娘</span>
+			<span>小姐姐</span>
 		</div>`);
 	const toggle = document.getElementById("waifu-toggle");
 	toggle.addEventListener("click", () => {
