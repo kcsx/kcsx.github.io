04c9d33b6a282e102dd41d84e39f2ed6f11bc744
kaiyu <1865400033@.e.gzhu.edu.cn>
2021-05-07 20:21:19 +0800

    Site updated: 2021-05-07 20:21:18

---
+++ js/artitalkkey.js
@@ -0,0 +1,15 @@
+new Artitalk({
+    appId: 'Hg****Nqlisn3Uce****nctL-Md***MMI',  //替换为你的appId
+    appKey: 'NTR******T2pr****XVbDMyt', //替换为你的appKey
+    pageSize: 1,
+    color1: "#49b1f5",
+    color2: "#00c4b6",
+    serverURL: "https://Hg****Nq.api.lncldglobal.com",
+    //Hg****Nq替换为你的appId前八位
+});
+$(".power").css("padding", "10px");
+$(".power").css("text-align", "left");
+$("#switchUser").css("padding-right", "10px");
+$("#pubShuo").css("padding-right", "10px");
+$("#pubComment").css("padding-right", "10px");
+$("#uploadSource").css("padding-right", "10px");
